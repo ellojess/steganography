8432df4500161cabf4236561c2ed4549b8a27771
Jess <jtrinh619@gmail.com>
2021-04-26 16:46:36 -0700

    [review] implementation of encode_image and write_text

--- decode_image.py
+++ decode_image.py
@@ -45,15 +45,65 @@ def decode_image(path_to_png):
 path_to_png = "doggo_encoded.png"
 decode_image(path_to_png)
 
-def encode_image(path_to_png):
+
+def encode_image(text_to_encode, image_to_encode):
+    """
+    Encode message into an image 
+    """
+
+    # Open the image using PIL:
+    image_to_encode = Image.open(image_to_encode)
+
+    # Split the different color channels
+    red_channel = image_to_encode.split()[0]
+    green_channel = image_to_encode.split()[1]
+    blue_channel = image_to_encode.split()[2]
+
+    # encode text into image 
+    x_size, y_size = image_to_encode
+    encoded_image = Image.new("RGB", (x_size, y_size))
+    pixels = encoded_image.load()
+
+    # text draw 
+    image_text = write_text(text_to_encode, image_to_encode.s)
+    black_white_encode = image_text.convert('1')
+
+    for x in range(x_size):
+        for y in range(y_size):
+            red_channel_pix = bin(red_channel.getpixel(x, y))
+            original_pix = red_channel_pix.getpixel((x, y))
+            tencode_pix = bin(black_white_encode.getpixel(x, y))
+
+            if tencode_pix[-1] == '1':
+                red_channel_pix = red_channel_pix[:-1] + ''
+            else :
+                red_channel_pix = red_channel_pix[:-1] + ''
+            
+            pixels[x, y] = (int(red_channel_pix, 2), green_channel.getpixel(x, y), blue_channel.getpixel(x, y)) 
+            
+    encoded_image.save("encoded_image.png")
+
+
+
+
+def write_text(text_to_write, image_size):
     """
     TODO: Add docstring and complete implementation.
     """
-    pass
+ 
+    image_text = Image.new("RGB", image_size)
+    font = ImageFont.load_default().font
+    drawer = ImageDraw.Draw(image_text)
+
+    # Text wrapping. Change parameters for different text formatting
+    margin = offset = 10
+
+    for line in textwrap.wrap(text_to_write, width=60):
+        drawer.text((margin,offset), line, font=font)
+        offset += 10
+    return image_text
 
 
-def write_text(text_to_write):
-    """
-    TODO: Add docstring and complete implementation.
-    """
-    pass
+if __name__ == '__main__':
+    encode_image("hello world", "doggo_encoded.png")
+    decode_image()
